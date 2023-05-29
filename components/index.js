1c9628dda4edf599848222953b9834f0c0302bf4
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-29 10:50:22 -0600

    create barrel

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export { default as HeaderLeft } from './HeaderLeft/HeaderLeft';
+export { default as HeaderRight } from './HeaderRight/HeaderRight';
+export { default as Navbar } from './Navbar/Navbar';
