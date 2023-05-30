1b5eb6589276b65bfef240befcbe43630c778555
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-29 18:08:08 -0600

    add component to barrel

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
+export { default as BannerScreen } from './BannerScreen/BannerScreen';
 export { default as HeaderLeft } from './HeaderLeft/HeaderLeft';
 export { default as HeaderRight } from './HeaderRight/HeaderRight';
 export { default as Navbar } from './Navbar/Navbar';
+export { default as SearchScreen } from './SearchScreen/SearchScreen';
